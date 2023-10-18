c88782308f618b8aed034423c07206725899a4a8
Federico Urones <federico.urones@mitrol.net>
2023-10-18 09:20:03 -0300

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import CoolBackground from "./CoolBackground/index.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CoolBackground />
     <App />
+    <Analytics />
   </React.StrictMode>
 );
